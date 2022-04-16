9466e60fc0b3b0747954f7f34d3f0732b38e736a
Guilherme Viana <guilhermehviana01@gmail.com>
2022-04-16 10:59:00 -0300

    Add Faq list behavior

--- dist/script.js
+++ dist/script.js
@@ -1,26 +1,36 @@
 "use strict";
-const setDarkMode = function () {
-    const pageBody = document.querySelector('body');
+var setDarkMode = function () {
+    var pageBody = document.querySelector('body');
     pageBody === null || pageBody === void 0 ? void 0 : pageBody.classList.toggle('dark-mode');
 };
-const darkModeEventListener = function () {
-    const setDarkModeElement = document.querySelector('.set-darkmode');
+var darkModeEventListener = function () {
+    var setDarkModeElement = document.querySelector('.set-darkmode');
     setDarkModeElement === null || setDarkModeElement === void 0 ? void 0 : setDarkModeElement.addEventListener('click', setDarkMode);
 };
-const setCityTab = function (index) {
-    const tabContent = document.querySelectorAll('.city-description');
+var setCityTab = function (index) {
+    var tabContent = document.querySelectorAll('.city-description');
     tabContent.forEach(function (section) { return section.classList.remove('active'); });
     tabContent[index].classList.add('active');
 };
-const citiesTabEventListener = function () {
-    const citiesTabMenu = document.querySelectorAll('.js-tabmenu li');
+var citiesTabEventListener = function () {
+    var citiesTabMenu = document.querySelectorAll('.js-tabmenu li');
     citiesTabMenu === null || citiesTabMenu === void 0 ? void 0 : citiesTabMenu.forEach(function (city, index) {
         city.addEventListener('click', function () { return setCityTab(index); });
     });
 };
-const init = function () {
+var setFaqDescription = function (index) {
+    var _a;
+    var faqDescriptions = document.querySelectorAll('.faq-description');
+    (_a = faqDescriptions[index]) === null || _a === void 0 ? void 0 : _a.classList.toggle('active-accordion');
+};
+var faqTitleEventListener = function () {
+    var faqDescriptionTitles = document.querySelectorAll('.faq-list dt');
+    faqDescriptionTitles
+        .forEach(function (dt, index) { return dt.addEventListener('click', function () { return setFaqDescription(index); }); });
+};
+var init = function () {
     darkModeEventListener();
     citiesTabEventListener();
+    faqTitleEventListener();
 };
-
 window.onload = function () { return init(); };
--- script.ts
+++ script.ts
@@ -1,22 +1,22 @@
-const setDarkMode = () => {
+const setDarkMode = (): void => {
   const pageBody: HTMLElement | null = document.querySelector('body');
   pageBody?.classList.toggle('dark-mode');
 };
 
-const darkModeEventListener = () => {
+const darkModeEventListener = (): void => {
   const setDarkModeElement: HTMLElement | null = document.querySelector('.set-darkmode');
 
   setDarkModeElement?.addEventListener('click', setDarkMode);
 }
 
 
-const setCityTab = (index: number) => {
+const setCityTab = (index: number): void => {
   const tabContent: NodeListOf<Element> | null = document.querySelectorAll('.city-description');
   tabContent.forEach((section) => section.classList.remove('active'))
   tabContent[index].classList.add('active');
 }
 
-const citiesTabEventListener = () => {
+const citiesTabEventListener = (): void => {
   const citiesTabMenu: NodeListOf<Element> | null = document.querySelectorAll('.js-tabmenu li');
 
   citiesTabMenu?.forEach((city: Element, index) => {
@@ -24,10 +24,24 @@ const citiesTabEventListener = () => {
   });
 }
 
-const init = () => {
+const setFaqDescription = (index: number): void => {
+  const faqDescriptions = document.querySelectorAll('.faq-description')
+  faqDescriptions[index]?.classList.toggle('active-accordion');
+}
+
+const faqTitleEventListener = (): void => {
+  const faqDescriptionTitles: NodeListOf<Element> = document.querySelectorAll('.faq-list dt');
+
+  faqDescriptionTitles
+    .forEach((dt, index) => dt.addEventListener('click', () => setFaqDescription(index)));
+}
+
+const init = (): void => {
   darkModeEventListener();
 
   citiesTabEventListener();
+
+  faqTitleEventListener();
 }
 
 window.onload = () => init();
